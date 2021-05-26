bda2350bf0941f272f36942d361aec127edfd499
Gabriel Morales <gabrielmorales201564@hotmail.com>
2021-05-26 13:01:07 -0500

    lo que dio quiera

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const argv   = require('./config/yargs').argv;
-const  {mostrar} = require('./buscador/buscar');
+const argv = require("./config/yargs").argv;
+const { mostrar,guardar } = require("./buscador/buscar");
 
-const  command = argv._[0];
+const command = argv._[0];
 
-switch(command){
-    case 'mostrar':
-        mostrar(argv.country, argv.year,argv.file).catch(resp=>{
-            console.log(resp);
-        });
-        break;
-    default:
-        console.log('Error');
-
+switch (command) {
+  case "mostrar":
+    mostrar(argv.country, argv.year, argv.file).catch((resp) => {
+      console.log(resp);
+    });
+    break;
+  case "guardar":
+    guardar(argv.country, argv.year, argv.file)
+      .then((datosGuardados) => {
+        console.log(datosGuardados);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    break;
+  default:
+    console.log("Error");
 }
--- buscador/buscar.js
+++ buscador/buscar.js
@@ -31,7 +31,50 @@ const mostrar = (country , year , path = "./datos.csv") => {
   });
 };
 
+const resp = `${process.cwd()}/resultados`;
+const guardar = (country = 'ECU', year = 1960, path = './datos.csv') => {
+  return new Promise((resolve, reject) => {
+    if (!Number(year)) {
+      reject(`El valor introducido '${year}' no es un número`);
+      return;
+    }
+
+    let saveData = '';
+
+    fs.createReadStream(path)
+      .pipe(csv({}))
+      .on('data', data => res.push(data))
+      .on('end', () => {
+        for (let i = 0; i < res.length; i++) {
+          if (res[i]['Country Code'] === country) {
+            data = res[i];
+            saveData += `Datos: ${data['Indicator Name']} 
+                                Pais: ${data['Country Name']} 
+                                Año: ${year} 
+                                Valor: ${data[year]} \n`;
+          }
+        }
+        if (!resp) {
+          fs.writeFile(`./resultados/${country}-${country}.txt`, saveData, err => {
+            if (err) reject(err);
+            resolve(`Archivo guardado exitosamente: resultados/${country}-${country}.txt`);
+          });
+        } else {
+          fs.mkdir(resp, err => {
+            if (err) {
+              console.error('Guardando en: resultados');
+            }
+          });
+          fs.writeFile(`./resultados/${country}-${country}.txt`, saveData, err => {
+            if (err) reject(err);
+            resolve(`Archivo guardado exitosamente: resultados/${country}-${country}.txt`);
+          });
+        }
+      });
+  });
+};
+
 module.exports = {
   mostrar,
-//   guardar,
+  guardar,
 };
